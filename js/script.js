fd7306067dfccc954edc0117d12dc9e2ad0edf40
TxsAdamWest <Twinadanz@gmail.com>
2016-03-22 03:58:05 -0500

    modified sounds and more style fixes, plus added randomizer fight

--- js/script.js
+++ js/script.js
@@ -9,58 +9,79 @@ document.querySelector('.startScreen1')
 
 var gameOver1 = document.querySelector('.gameOver1')
 var gameOver2 = document.querySelector('.gameOver2')
+var gameOver3 = document.querySelector('.gameOver3')
 
 // ------------ Life Bars ------------- //
 
+var lifeContainer = document.querySelector('span')
 
 var axeLifeBar = document.querySelector('.axeLifeBar')
+var axeLifeBarPercentWidth = 100
+
 var enemyLifeBar = document.querySelector('.enemyLifeBar')
-var lifeBarPercentWidth = 100
+var enemyLifeBarPercentWidth = 100
+
 
 // ------------ Sound Scripts ------------- //
 
 // This is an randomizer function to play random mp3's from an array of sounds.
 Array.prototype.choice = function() {
-    var randomIndex = Math.floor(Math.random() * this.length)
-    return this[randomIndex]
-}
-// ----------------------------------------- //
+        var randomIndex = Math.floor(Math.random() * this.length)
+        return this[randomIndex]
+    }
+    // ----------------------------------------- //
 
 // ------------ Button Scripts ------------- //
 
 
-var audioIds = ["#axeCullingBlade","#firstBlood","#doubleKill","#tripleKill","#rampage","#holyShit","#axeDeath"]
+var audioIds = ["#axeCullingBlade", "#firstBlood", "#svenWarcry1", "#svenWarcry2", "#svenStunned", "#axeWarcry1", "#axeWarcry2", "#axeDeath"]
+var victoryIds = ["#axeWins", "#enemyWins"]
 var idCounter = 0
 
 $(killButton).on('click', function() {
 
-	// var audioElId = audioIds.choice() // This will cycle through all sounds in audioIds
-	var audioElId = audioIds[idCounter] // This will only cycle through starting on the 1st index.
-	var audioEl = document.querySelector(audioElId)
-	idCounter += 1
+    // var audioElId = audioIds.choice() // This will cycle through all sounds in audioIds
+    var audioElId = audioIds[idCounter] // This will only cycle through starting on the 1st index.
+    var audioEl = document.querySelector(audioElId)
+    idCounter += 1
 
-	lifeBarPercentWidth -= 20 
-	axeLifeBar.style.width = lifeBarPercentWidth + '%'
+    axeLifeBarPercentWidth -= (Math.random() * 40)
+    axeLifeBar.style.width = axeLifeBarPercentWidth + '%'
 
-	$(".startScreen1").fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100); // Blinking effect when hit.
+    enemyLifeBarPercentWidth -= (Math.random() * 40)
+    enemyLifeBar.style.width = enemyLifeBarPercentWidth + '%'
 
-	// console.log(lifeBar.offsetWidth)
+    $(".startScreen1").fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100); // Blinking effect when hit.
+    // $(".startScreen2").fadeIn(200).fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200);
 
-	if ( lifeBarPercentWidth < 0) {
-		gameOver1.style.display = 'block' 
-	}
+    // console.log(lifeBar.offsetWidth)
+
+    if (axeLifeBarPercentWidth < 0) {
+        var audioElId = victoryIds[1] // This will only cycle through starting on the 1st index.
+        var audioEl = document.querySelector(audioElId)
+        lifeContainer.style.display = 'none'
+        gameOver2.style.display = 'block'
+    }
+
+    if (enemyLifeBarPercentWidth < 0) {
+        var audioElId = victoryIds[0] // This will only cycle through starting on the 1st index.
+        var audioEl = document.querySelector(audioElId)
+        lifeContainer.style.display = 'none'
+        gameOver1.style.display = 'block'
+    }
 
     audioEl.play()
 })
 
 $(winButton).on('click', function() {
 
-	var audioElId = audioIds[6] // This will only cycle through starting on the 1st index.
-	var audioEl = document.querySelector(audioElId)
+    var audioElId = audioIds[7] // This will only cycle through starting on the 1st index.
+    var audioEl = document.querySelector(audioElId)
 
-	gameOver2.style.display = 'block'
+    lifeContainer.style.display = 'none'
+    gameOver3.style.display = 'block'
 
-	audioEl.play()
+    audioEl.play()
 })
 
 // ------------ BG Music ------------- //
@@ -69,4 +90,3 @@ window.onload = function() {
     var audioEl = document.querySelector('#dota2theme')
     audioEl.play()
 }
-
